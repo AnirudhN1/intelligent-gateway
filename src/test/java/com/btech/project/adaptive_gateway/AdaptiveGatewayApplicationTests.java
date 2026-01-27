669cef72594a9c4f7601cc26c8a79da0b3a2f699
AnirudhN1 <anirudhn26@gmail.com>
2026-01-27 19:15:32 +0530

    chore: initial project skeleton with Spring Boot 4.0.2

---
+++ src/test/java/com/btech/project/adaptive_gateway/AdaptiveGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btech.project.adaptive_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdaptiveGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
